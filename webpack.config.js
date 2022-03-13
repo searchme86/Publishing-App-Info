d1b1e36de97ff384774280b1230b73da2ecb99b6
searchme86 <searchme86@gmail.com>
2022-03-13 20:47:53 +0900

    🔧  add config files

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  entry: './src/scripts/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    clean: true,
+  },
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    open: true,
+    watchFiles: 'index.html',
+  },
+  devtool: 'source-map',
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Portfolio',
+      template: './index.html',
+      inject: 'body',
+      // favicon: './favicon.ico',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new TerserWebpackPlugin(), new CssMinimizerPlugin()],
+  },
+};
